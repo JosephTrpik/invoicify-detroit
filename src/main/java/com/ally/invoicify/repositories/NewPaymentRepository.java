12e8c9529ae0a948b4efba7fdb4fad53418a6acc
ZZ32CW@ALLYIPOC.COM <ZZ32CW@ALLYIPOC.COM>
2019-07-24 10:31:55 -0400

    add find by invoice and fix add new payment

--- src/main/java/com/ally/invoicify/repositories/NewPaymentRepository.java
+++ src/main/java/com/ally/invoicify/repositories/NewPaymentRepository.java
@@ -1,9 +1,13 @@
 package com.ally.invoicify.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ally.invoicify.models.NewPayment;
 
 public interface NewPaymentRepository extends JpaRepository<NewPayment, Long> {
+
+    List<NewPayment> findAllByInvoiceId(long invoiceId);
 
 }
